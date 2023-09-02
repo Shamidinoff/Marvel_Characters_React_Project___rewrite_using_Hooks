b2b1e7939ae9092a45401f6322dd6d569453b5c2
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-09-02 12:39:31 +0600

    Rewrire the entire project using Hooks

---
+++ src/components/app/App.js
@@ -0,0 +1,45 @@
+import { Component } from "react/cjs/react.production.min";
+
+import AppHeader from "../appHeader/AppHeader";
+import RandomChar from "../randomChar/RandomChar";
+import CharList from "../charList/CharList";
+import CharInfo from "../charInfo/CharInfo";
+import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+
+import decoration from "../../resources/img/vision.png";
+
+class App extends Component {
+  onCharSelected = (id) => {
+    this.setState({
+      selectedChar: id,
+    });
+  };
+
+  state = {
+    selectedChar: null,
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <AppHeader />
+        <main>
+          <ErrorBoundary>
+            <RandomChar />
+          </ErrorBoundary>
+          <div className="char__content">
+            <ErrorBoundary>
+              <CharList onCharSelected={this.onCharSelected} />
+            </ErrorBoundary>
+            <ErrorBoundary>
+              <CharInfo charId={this.state.selectedChar} />
+            </ErrorBoundary>
+          </div>
+          <img className="bg-decoration" src={decoration} alt="vision" />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;

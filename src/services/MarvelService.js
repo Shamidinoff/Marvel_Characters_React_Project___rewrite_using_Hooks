e8490ddf32c98682b70de2189eb4b4d84f67b3c2
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-09-05 22:44:36 +0600

    Rewrite the entire project using Hooks

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -31,7 +31,7 @@ const useMarvelService = () => {
     return res.data.results.map(_transformComics);
   };
 
-  const getComics = async (id) => {
+  const getComic = async (id) => {
     const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
     return _transformComics(res.data.results[0]);
   };
@@ -72,7 +72,7 @@ const useMarvelService = () => {
     getAllCharacters,
     getCharacter,
     getAllComics,
-    getComics,
+    getComic,
   };
 };
 
